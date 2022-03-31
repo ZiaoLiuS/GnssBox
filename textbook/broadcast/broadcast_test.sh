a4d90aca40c78c127c27572d2427cc56cc470a08
ZiaoLiuS <102714033+ZiaoLiuS@users.noreply.github.com>
2022-03-31 13:35:10 +0800

    Delete textbook directory

--- textbook/broadcast/broadcast_test.sh
+++
@@ -1,1 +0,0 @@
-../../cmake-build-debug/apps/broadcast_test  --nav ./BRDC00WRD_R_20210010000_01D_MN.rnx  --sp3 ./WUM0MGXFIN_20210010000_01D_15M_ORB.SP3  --output ./result.txt 
--- textbook/getopt/getopt_test.sh
+++
@@ -1,1 +0,0 @@
-../../cmake-build-debug/apps/getopt3_test --input ./test.txt --input ./test01.txt --input ./test02.txt --output ./result.txt 
